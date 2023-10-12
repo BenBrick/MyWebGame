6034f54864fc57fa5c678a1c0cc4abf357fdcf95
BenBrick <113672740+BenBrick@users.noreply.github.com>
2023-10-12 14:11:53 +0000

    Light

--- Resources/js/game.js
+++ Resources/js/game.js
@@ -1,33 +1,31 @@
 import * as THREE from "three";
 
 
-// creates a scene and camera
-const scene = new THREE.Scene();
-//FOV, Aspect Ratio, Near Clipping Plane, Far Clipping Plane
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene(); // creates a scene
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); //craetes a camera and sets the FOV, Aspect Ratio, Near Clipping Plane, Far Clipping Plane
 
-//sets the size of the renderer 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-// adds a canvas for the renderer to display the scene
-document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer(); // craetes a renderer
+renderer.setSize(window.innerWidth, window.innerHeight); // sets the size of the renderer
+document.body.appendChild(renderer.domElement); // adds a canvas for the renderer to display the scene
 
 // creates the cube that will be rendered in the scene
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.BoxGeometry(1, 1, 1); //size
+const material = new THREE.MeshStandardMaterial({ color: 0x0000ff }); //colour
+const cube = new THREE.Mesh(geometry, material); //combines the geometry and material
+scene.add(cube); //adds the cube to the scene
+const light = new THREE.AmbientLight( 0xfdfeff,1 ); // creates a soft white light
+scene.add(light);
 
-camera.position.z = 5;
+camera.position.z = 5; //sets the camera position
 
 // animates the cube by rotating it
 function animate() {
-  requestAnimationFrame(animate);
+  requestAnimationFrame(animate); //calls the animate function every frame
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01; //rotates the cube on the x axis
+  cube.rotation.y += 0.01; //rotates the cube on the y axis
 
-  renderer.render(scene, camera);
+  renderer.render(scene, camera); //renders the scene and camera
 }
 
-animate();
+animate(); //calls the animate function
